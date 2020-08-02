584ed8a597e52254fd70cea49af9fd2900348bc3
Charlie Moore <zygenhideo@gmail.com>
2020-08-01 17:41:14 -0700

    Remove redundant "global" style configuration

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -1,5 +1,29 @@
 <style global lang="postcss">
   @tailwind base;
+
+  body {
+    @apply font-sans;
+  }
+
+  label {
+    display: block;
+  }
+
+  input,
+  button,
+  select,
+  textarea {
+    font-family: inherit;
+    font-size: inherit;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+  }
+
+  button:disabled {
+    color: #999;
+  }
+
   @tailwind components;
   @tailwind utilities;
 </style>
